fddb6bdb9d0804c534948316214a5af8edc902eb
Brian Berg <brian.berg@cgi.com>
2017-09-12 16:45:09 -0600

    updating application yaml, adding manifest

--- src/main/java/com/cgi/bbergcloudserver/BbergCloudServerApplication.java
+++ src/main/java/com/cgi/bbergcloudserver/BbergCloudServerApplication.java
@@ -2,10 +2,12 @@ package com.cgi.bbergcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BbergCloudServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BbergCloudServerApplication.class, args);
 	}
