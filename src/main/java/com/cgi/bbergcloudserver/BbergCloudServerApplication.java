457ba7acee4aefdfd14429ddf16c0996c5d21039
Brian Berg <brian.berg@cgi.com>
2017-09-12 16:14:59 -0600

    initial commit

---
+++ src/main/java/com/cgi/bbergcloudserver/BbergCloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.cgi.bbergcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbergCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbergCloudServerApplication.class, args);
+	}
+}
