d06a9bd72a013dde7130e222b67d3b9e14096f75
Brian Berg <brian.berg@cgi.com>
2017-09-13 14:50:23 -0600

    remove eureka

--- src/main/java/com/cgi/bbergcloudserver/BbergCloudServerApplication.java
+++ src/main/java/com/cgi/bbergcloudserver/BbergCloudServerApplication.java
@@ -2,12 +2,10 @@ package com.cgi.bbergcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class BbergCloudServerApplication {
 	
 	public static void main(String[] args) {
